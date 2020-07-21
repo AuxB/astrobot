0811f7476d87d7046c4b53b24dc7e73bf7cffcca
Auxence <auxence.blondel@gmail.com>
2020-07-21 19:42:56 +0200

    set up express and ngrok to work on local

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
-const functions = require('firebase-functions');
+// 'use strict'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// const http = require('http')
+// const functions = require('firebase-functions')
+// const { WebhookClient } = require('dialogflow-fulfillment')
+
+// // admin.initializeApp(functions.config().firebase);
+// // const db = admin.firestore()
+
+// exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
+//   // const agent = new WebhookClient({ req, res })
+//   functions.logger.info('Hello logs!', { structuredData: true })
+//   // const bodyName = req.body.queryResult.parameters['body-name']
+//   res.send('bichon')
+// })
+
+const express = require('express')
+const { WebhookClient } = require('dialogflow-fulfillment')
+const app = express()
+
+app.get('/', (req, res) => res.send('online'))
+app.post('/dialogflow', express.json(), (req, res) => {
+
+  
+})
+
+app.listen(process.env.PORT || 8080)
