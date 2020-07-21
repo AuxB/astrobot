407cb61016956531d9a500e54314cfe492ba6aee
Auxence <auxence.blondel@gmail.com>
2020-07-22 00:32:40 +0200

    modifi index.js to add configuration for firebase deployement

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,16 @@
-// 'use strict'
+// @flow
 
-// const http = require('http')
-// const functions = require('firebase-functions')
+'use strict'
+
 // const { WebhookClient } = require('dialogflow-fulfillment')
-
-// // admin.initializeApp(functions.config().firebase);
-// // const db = admin.firestore()
-
-// exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
-//   // const agent = new WebhookClient({ req, res })
-//   functions.logger.info('Hello logs!', { structuredData: true })
-//   // const bodyName = req.body.queryResult.parameters['body-name']
-//   res.send('bichon')
-// })
 
 const express = require('express')
 // const { WebhookClient } = require('dialogflow-fulfillment')
 const app = express()
 const http = require('https')
+const functions = require('firebase-functions')
 
-function getGlobalInfo (bodyName) {
+function getGlobalInfo(bodyName) {
   return new Promise((resolve, reject) => {
     const path = `https://api.le-systeme-solaire.net/rest/bodies/?filter[]=englishName,eq,${bodyName}&data=englishName,inclinaison,meanRadius,gravity,mass,massValue`
 
@@ -53,7 +44,8 @@ function getGlobalInfo (bodyName) {
 }
 
 app.get('/', (req, res) => res.send('online'))
-app.post('/dialogflow', express.json(), (req, res) => {
+// app.post('/dialogflow', express.json(), (req, res) => {
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
   // const agent = new WebhookClient({ request: req, response: res })
 
   const bodyName = req.body.queryResult.parameters['body-name']
@@ -68,6 +60,6 @@ app.post('/dialogflow', express.json(), (req, res) => {
   // agent.handleRequest(intentMap)
 })
 
-app.listen(process.env.PORT || 8080)
+// app.listen(process.env.PORT || 8080)
 
-module.exports = getGlobalInfo
+// module.exports = getGlobalInfo
