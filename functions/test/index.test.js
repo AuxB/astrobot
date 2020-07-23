325ec2c45cf2d2515c228f53138bad23bb4b1d20
Auxence <auxence.blondel@gmail.com>
2020-07-23 11:14:44 +0200

    implemente the first test to verify type of request and response

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,9 +1,32 @@
-// const assert = require('assert');
-// const getGlobalInfo = require('../index');
+const { dialogflowFirebaseFulfillment } = require('../src/index')
+const { assert } = require('chai')
 
-// describe('getGlobalInfo', () => {
-//   it('is a function and accepting one argument', () => {
-//     assert.strictEqual(typeof getGlobalInfo, 'function');
-//     assert.strictEqual(getGlobalInfo.length, 1);
-//   });
-// });
+const request = (bodyName, intentType) => {
+    return {
+        body: {
+            queryResult: {
+                parameters: {
+                    'body-name': bodyName
+                },
+                intent: {
+                    'displayName': intentType
+                }
+            }
+        }
+    }
+}
+
+describe('dialogflowFirebaseFulfillment', () => {
+    it('The response is type of object and his text is a string', () => {
+        const req = request('moon', 'get-is-planet')
+        assert.typeOf(req.body.queryResult.parameters['body-name'], 'string')
+        const res = {
+            json: (resBody) => {
+                assert.typeOf(resBody, 'object')
+                assert.typeOf(resBody.fulfillmentText, 'string')
+                console.log(resBody)
+            }
+        }
+        dialogflowFirebaseFulfillment(req, res)
+    });
+});
