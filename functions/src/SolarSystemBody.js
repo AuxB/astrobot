1db09a26c979bd05d1c9d4b594efb76bbea8b2e1
auxb <auxence.blondel@gmail.com>
2020-08-12 16:50:16 +0200

    export type SolarBody to use in multiple functions

---
+++ functions/src/SolarSystemBody.js
@@ -0,0 +1,11 @@
+// @flow
+/**
+* @global - Type use for fullfiled data bodies
+*/
+export type SolarSystemBody = {
+    name: string,
+    mass: string,
+    radius: string,
+    gravity: string,
+    isPlanet: boolean,
+};
